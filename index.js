3ae3d2247cc952f4978449c4f361b72acbe4694d
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-06-08 09:16:01 -0700

    Add files via upload
    Adding a OOP My Bank Pro-11

---
+++ index.js
@@ -0,0 +1,111 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Bank account Class
+class BankAccount {
+    accountNumber;
+    balance;
+    constructor(accountNumber, balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    // Debit money
+    withdraw(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(`Withdrawal of $${amount} successful. Remaining balance: ${this.balance}`);
+        }
+        else {
+            console.log("Insufficient balance.");
+        }
+    }
+    // Credit money
+    deposit(amount) {
+        if (amount > 100) {
+            amount -= 1; // $1 fee charged if more than $100 is deposited
+        }
+        this.balance += amount;
+        console.log(`Deposit of $${amount} successful. Remaining balance: $${this.balance}`);
+    }
+    // Check balance 
+    checkBalance() {
+        console.log(`Current balance: $${this.balance}`);
+    }
+}
+//  Customer class 
+class Customer {
+    firstName;
+    lastName;
+    gender;
+    age;
+    mobileNumber;
+    account;
+    constructor(firstName, lastName, gender, age, mobileNumber, account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.account = account;
+    }
+}
+// Create bank accounts
+const accounts = [
+    new BankAccount(1001, 500),
+    new BankAccount(1002, 1000),
+    new BankAccount(1003, 2000)
+];
+// Create customers
+const customers = [
+    new Customer("Hamza", "Khan", "Male", 35, 316222334, accounts[0]),
+    new Customer("shazia", "Khan", "Female", 24, 333222334, accounts[1]),
+    new Customer("Sara", "Khan", "Female", 30, 300222334, accounts[2])
+];
+// Function to interract with bank account
+async function service() {
+    do {
+        const accountNumberInput = await inquirer.prompt({
+            name: "accountNumber",
+            type: "number",
+            message: "Enter your account number:"
+        });
+        const customer = customers.find(customer => customer.account.accountNumber === accountNumberInput.accountNumber);
+        if (customer) {
+            console.log(`Welcome, ${customer.firstName} ${customer.lastName}!\n`);
+            const ans = await inquirer.prompt([{
+                    name: "select",
+                    type: "list",
+                    message: "Select an operation",
+                    choices: ["Deposit", "Withdraw", "Check Balance", "Exit"]
+                }]);
+            switch (ans.select) {
+                case "Deposit":
+                    const depositAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to Deposit:"
+                    });
+                    customer.account.deposit(depositAmount.amount);
+                    break;
+                case "Withdraw":
+                    const withdrawAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to Withdraw:"
+                    });
+                    customer.account.deposit(withdrawAmount.amount);
+                    break;
+                case "Check Balance":
+                    customer.account.checkBalance();
+                    break;
+                case "Exit":
+                    console.log("Exiting bank program...");
+                    console.log("\n Thank you for using our Bank services. Have a great day!");
+                    return;
+            }
+        }
+        else {
+            console.log("Invalid account number. Please try again.");
+        }
+    } while (true);
+}
+service();
